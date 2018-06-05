dbe592afa44f34f818f0809541048998d3f40ae2
siasun <liuhao257@163.com>
2018-06-05 16:57:34 +0800

    sdfasdf

--- GesturePushAndPop/GesturePushAndPop/tab1_探索/CommentTableView.m
+++ GesturePushAndPop/GesturePushAndPop/tab1_探索/CommentTableView.m
@@ -11,7 +11,8 @@
 @interface CommentTableView()<UITableViewDataSource,UITableViewDelegate>
 
 @property (nonatomic, assign) BOOL isDragging;
-@property (nonatomic, assign) CGPoint lastContentOffset;
+@property (nonatomic, strong) UIView *headerView;
+@property (nonatomic, strong) UITextField *footerView;
 @end
 
 @implementation CommentTableView
@@ -36,37 +37,53 @@
 static CGPoint originPoint;
 static CGRect originRect;
 static CGPoint beginTouchPoint;
+
+static CGPoint headerViewOriginPoint;
+static CGRect headerViewOriginRect;
+
 - (void)commonInit
 {
-    _isDragging = NO;
+    self.isDragging = NO;
     originPoint = CGPointMake(0, 200);
     originRect = CGRectMake(0, 200, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height - 200);
     
+    headerViewOriginPoint = CGPointMake(0, 170);
+    headerViewOriginRect = CGRectMake(0, 170, [UIScreen mainScreen].bounds.size.width, 30);
+    
     self.delegate = self;
     self.dataSource = self;
+    
+    self.headerView = [[UIView alloc] init];
+    self.headerView.frame = headerViewOriginRect;
+    self.headerView.backgroundColor = [UIColor blackColor];
+    
+    self.footerView = [[UITextField alloc] initWithFrame:CGRectMake(0, [UIScreen mainScreen].bounds.size.height-40, [UIScreen mainScreen].bounds.size.width, 40)];
+    self.footerView.backgroundColor = [UIColor blackColor];
 }
 
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView
 {
     if (scrollView.contentOffset.y <= 0 ||
-        scrollView.contentOffset.y - self.lastContentOffset.y > 0) {
+        self.isDragging == YES) {
         [self panGestureRecognizers:scrollView.panGestureRecognizer];
     }
-    self.lastContentOffset = scrollView.contentOffset;
 }
 
 - (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
 {
-    _isDragging = NO;
+    self.isDragging = NO;
     if (self.frame.origin.y - originPoint.y < 100) {
         [UIView animateWithDuration:0.2 animations:^{
             self.frame = originRect;
+            self.headerView.frame = headerViewOriginRect;
         }];
     } else {
         [UIView animateWithDuration:0.2 animations:^{
             self.frame = CGRectMake(0, [UIScreen mainScreen].bounds.size.height, originRect.size.width, originRect.size.height);
+            self.headerView.frame = CGRectMake(0, [UIScreen mainScreen].bounds.size.height, originRect.size.width, 30);
         } completion:^(BOOL finished) {
             [self removeFromSuperview];
+            [self.headerView removeFromSuperview];
         }];
     }
 }
@@ -76,18 +93,27 @@ static CGPoint beginTouchPoint;
     if (gestureRecognizer.state == UIGestureRecognizerStateBegan) {
         NSLog(@"not called");
     } else if (gestureRecognizer.state == UIGestureRecognizerStateChanged) {
-        if (_isDragging == NO) {
-            beginTouchPoint = [gestureRecognizer locationInView:self];
+        if (self.isDragging == NO) {
+            beginTouchPoint = [gestureRecognizer locationInView:self.superview];
         }
         
-        if (_isDragging == NO) {
-            _isDragging = YES;
+        if (self.isDragging == NO) {
+            self.isDragging = YES;
         }
         
-        CGPoint moveTouchPoint = [gestureRecognizer locationInView:self];
-        NSLog(@"%@",NSStringFromCGPoint(moveTouchPoint));
+        if (self.isDragging) {
+            [self setContentOffset:CGPointMake(0, 0)];
+        }
         
-        self.frame = CGRectMake(0, originPoint.y+(moveTouchPoint.y - beginTouchPoint.y) , originRect.size.width, originRect.size.height);
+        CGPoint moveTouchPoint = [gestureRecognizer locationInView:self.superview];
+        NSLog(@"y:%f",moveTouchPoint.y);
+        
+        if (originPoint.y+(moveTouchPoint.y - beginTouchPoint.y) >= originPoint.y) {
+            
+            self.frame = CGRectMake(0, originPoint.y+(moveTouchPoint.y - beginTouchPoint.y) , originRect.size.width, originRect.size.height);
+            self.headerView.frame = CGRectMake(0, headerViewOriginPoint.y+(moveTouchPoint.y - beginTouchPoint.y) , headerViewOriginRect.size.width, headerViewOriginRect.size.height);
+        }
+        
         
     } else if (gestureRecognizer.state == UIGestureRecognizerStateFailed) {
         NSLog(@"not called");
@@ -125,12 +151,30 @@ static CGPoint beginTouchPoint;
 {
     UIWindow *window = [UIApplication sharedApplication].keyWindow;
     [window addSubview:self];
-    self.frame = CGRectMake(0, 200, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height-200);
+    
+    self.frame = CGRectMake(0, [UIScreen mainScreen].bounds.size.height+30, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height-200);
+    self.headerView.frame = CGRectMake(0, [UIScreen mainScreen].bounds.size.height, originRect.size.width, 30);
+    [UIView animateWithDuration:0.2 animations:^{
+        self.frame = CGRectMake(0, 200, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height-200);
+        self.headerView.frame = headerViewOriginRect;
+    }];
+    
+    [window addSubview:self.headerView];
 }
 
 - (void)hide
 {
     [self removeFromSuperview];
+}
+
+- (void)setIsDragging:(BOOL)isDragging
+{
+    _isDragging = isDragging;
+    if (isDragging) {
+        self.showsVerticalScrollIndicator = NO;
+    } else {
+        self.showsVerticalScrollIndicator = YES;
+    }
 }
 
 @end
